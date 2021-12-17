9df800d8aac984b0969be221c549f32d87683c73
Carlos Arellano <carlosfarellano@hotmail.com>
2021-12-17 14:56:06 -0600

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,11 +13,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { AboutComponent } from './components/about/about.component';
 import { FooterComponent } from './components/footer/footer.component';
-
-const appRoutes: Routes = [
-  { path: '',  component: TasksComponent },
-  { path: 'about', component: AboutComponent }
-]
 
 @NgModule({
   declarations: [
@@ -36,8 +30,7 @@ const appRoutes: Routes = [
     AppRoutingModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot(appRoutes, { enableTracing: true  })
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -17,7 +17,7 @@ export class AddTaskComponent implements OnInit {
   subscription: Subscription;
 
   constructor(private uiService: UiService) { 
-    this.subscription = this.uiService.onToggle().subscribe( value => this.showAddTask = value)
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value);
   }
 
   ngOnInit(): void {
